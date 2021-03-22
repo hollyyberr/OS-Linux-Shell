dab1fae8bf5b9450c57517bc59b2debb304f3686
noahpaiva <78582698+noahpaiva@users.noreply.github.com>
2021-03-22 19:03:39 -0400

    Restart

--- execvp.c
+++
@@ -1,8 +0,0 @@
-#include<unistd.h>
-
-int main() {
-    char *argv[] = {"ls", "-l", "-h", "-a", NULL};
-    execvp(argv[0], argv);
-
-    return 0;
-}
---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include "shell.h"
+
+int main (int argc, char **argv) {
+
+    char *cmd;
+
+    do {
+        print_prompt1();
+    }
+}
